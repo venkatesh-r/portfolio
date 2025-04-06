afda004075645c2e48ed3d24705e40cf9f877bc2
venkatesh-r <venkypsd@gmail.com>
2025-04-06 22:27:07 +0200

    Skills test page

---
+++ src/components/Skills/__tests__/Skills.test.js
@@ -0,0 +1,27 @@
+import { BrowserRouter } from "react-router-dom";
+import { screen, render } from "@testing-library/react";
+import Skills from "../Skills";
+import "@testing-library/jest-dom";
+
+test("Heading renders", () => {
+  render(
+    <BrowserRouter>
+      <Skills />
+    </BrowserRouter>
+  );
+  expect(screen.getByText("Technical")).toBeInTheDocument();
+  expect(screen.getByText("Skills")).toBeInTheDocument();
+});
+
+test("Heading renders", () => {
+  render(
+    <BrowserRouter>
+      <Skills />
+    </BrowserRouter>
+  );
+  const paragraph = screen.getByText(
+    /My skills include HTML5, CSS3, SASS, JavaScript, jQuery, Angular, React JS, Node JS, Tailwind and MongoDB./
+  );
+
+  expect(paragraph).toBeInTheDocument();
+});
