ffda837035da2f0c706fbcf71067ff3606b07a0c
venkatesh-r <venkypsd@gmail.com>
2025-04-08 20:43:30 +0200

    Contact form testing

--- src/components/Contact/__tests__/Contact.test.js
+++ src/components/Contact/__tests__/Contact.test.js
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
@@ -27,4 +27,38 @@ test("paragraph renders", () => {
     /I'm interested in freelance opportunities, If you need any help just use the form./
   );
   expect(paragraph).toBeInTheDocument();
+});
+
+describe("contact form", () => {
+  test("fills and submit the contact form", () => {
+    render(
+      <BrowserRouter>
+        <Contact />
+      </BrowserRouter>
+    );
+
+    // Get all form fields
+    const nameInput = screen.getByPlaceholderText("Name");
+    const emailInput = screen.getByPlaceholderText("Email");
+    const subjectInput = screen.getByPlaceholderText("Subject");
+    const messageInput = screen.getByPlaceholderText("Message");
+    const sendButton = screen.getByDisplayValue("SEND");
+
+    //stimulate typing
+    fireEvent.change(nameInput, { target: { value: "Venkatesh" } });
+    fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+    fireEvent.change(subjectInput, { target: { value: "Test" } });
+    fireEvent.change(messageInput, {
+      target: { value: "This is test message" },
+    });
+
+    //check the input values
+    expect(nameInput.value).toBe("Venkatesh");
+    expect(emailInput.value).toBe("test@example.com");
+    expect(subjectInput.value).toBe("Test");
+    expect(messageInput.value).toBe("This is test message");
+
+    //Submit the form
+    fireEvent.click(sendButton);
+  });
 });
